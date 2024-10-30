4a67fe4fa69e091b227ac5cd9481e5f1d2040acc
Guido Pereira <stereopt@gmail.com>
2024-10-30 15:37:21 +0000

    refactor: nest project rename

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'coin-guard',
+  displayName: 'api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/coin-guard',
+  coverageDirectory: '../../coverage/apps/api',
 };
