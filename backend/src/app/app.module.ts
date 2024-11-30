c1c06b06ac56aa9ff8d71ab4a51774e4a31e753c
Guido Pereira <stereopt@gmail.com>
2024-11-30 00:02:30 +0000

    feat: typeorm connection

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -10,10 +10,33 @@ import {
 } from 'nest-winston';
 import * as winston from 'winston';
 import { LoggerMiddleware } from '../common/middleware/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig } from 'src/config/app.config';
 
 @Module({
   imports: [
     TransactionsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          entities: [],
+          synchronize: configService.get('database.synchronize'),
+          port: configService.get('database.port'),
+          username: configService.get('database.username'),
+          password: configService.get('database.password'),
+          host: configService.get('database.host'),
+          database: configService.get('database.name'),
+        };
+      },
+    }),
     WinstonModule.forRoot({
       transports: [
         new winston.transports.Console({
