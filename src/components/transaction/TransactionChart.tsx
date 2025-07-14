033d9b71fde63192d3e097141be155b76587b93b
StereoPT <stereopt@gmail.com>
2025-07-14 11:36:38 +0100

    feat: area graph

--- src/components/transaction/TransactionChart.tsx
+++ src/components/transaction/TransactionChart.tsx
@@ -1,11 +1,20 @@
 import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from '@/components/ui/chart';
 import { Transaction } from '@/generated/prisma';
-import { CartesianGrid, Line, LineChart, XAxis } from 'recharts';
+import { format } from 'date-fns';
+import { pt } from 'date-fns/locale';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
 
 const chartConfig = {
   date: {
@@ -30,48 +39,60 @@ export const TransactionChart = ({ transactions }: TransactionChartProps) => {
     return { date: t.date, amount: t.amount };
   });
 
+  const sum = transactions.reduce((acc, t) => (acc += t.amount), 0);
+  const average = sum / transactions.length;
+
   return (
-    <div className="border rounded-lg shadow-md overflow-auto">
-      <ChartContainer
-        config={chartConfig}
-        className="aspect-auto h-[250px] w-full">
-        <LineChart
-          accessibilityLayer
-          data={transactionData}
-          margin={{ left: 12, right: 12 }}>
-          <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="date"
-            tickLine={false}
-            axisLine={false}
-            tickMargin={8}
-            minTickGap={32}
-            tickFormatter={(value) => {
-              const date = new Date(value);
-              return date.toLocaleDateString('pt-PT', {
-                month: 'short',
-                day: 'numeric',
-              });
-            }}
-          />
-          <ChartTooltip
-            content={
-              <ChartTooltipContent
-                className="w-[150px]"
-                nameKey="date"
-                labelFormatter={(value) => {
-                  const date = new Date(value);
-                  return date.toLocaleDateString('pt-PT', {
-                    month: 'short',
-                    day: 'numeric',
-                  });
-                }}
-              />
-            }
-          />
-          <Line dataKey="amount" type="monotone" strokeWidth={2} dot={false} />
-        </LineChart>
-      </ChartContainer>
-    </div>
+    <Card className="py-4 sm:py-0">
+      <CardHeader className="flex flex-col items-stretch border-b !p-0 sm:flex-row">
+        <div className="flex flex-1 flex-col justify-center gap-1 px-6 pb-3 sm:pb-0">
+          <CardTitle>Transaction Graph</CardTitle>
+          <CardDescription>
+            Showing transaction amount over time
+          </CardDescription>
+        </div>
+        <div className="flex">
+          <div className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l sm:border-t-0 sm:border-l sm:px-8 sm:py-6">
+            <span className="text-muted-foreground text-xs">Sum</span>
+            <span className="text-lg leading-none font-bold sm:text-3xl">
+              {sum.toFixed(2)}€
+            </span>
+          </div>
+          <div className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l sm:border-t-0 sm:border-l sm:px-8 sm:py-6">
+            <span className="text-muted-foreground text-xs">Average</span>
+            <span className="text-lg leading-none font-bold sm:text-3xl">
+              {average.toFixed(2)}€
+            </span>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="px-2 sm:p-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full">
+          <AreaChart
+            accessibilityLayer
+            data={transactionData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => format(value, 'P', { locale: pt })}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Area dataKey="amount" type="monotone" fillOpacity={0.4} />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
   );
 };
--- src/components/transaction/UserTransaction.tsx
+++ src/components/transaction/UserTransaction.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { Sigma } from 'lucide-react';
-import { DisplayCard } from '@/components/DisplayCard';
 import { useGetTransaction } from '@/hooks/transaction/useGetTransaction';
 import { ErrorAlert } from '@/components/ErrorAlert';
 
@@ -23,23 +21,8 @@ export const UserTransaction = ({ transactionId }: UserTransactionProps) => {
     return <ErrorAlert />;
   }
 
-  const sum = transaction.all.reduce((acc, t) => (acc += t.amount), 0);
-  const average = sum / transaction.all.length;
-
   return (
     <div className="flex flex-col gap-4">
-      <div className="grid gap-4 md:grid-cols-4">
-        <DisplayCard title="Sum" icon={Sigma}>
-          <div className="flex gap-2 items-center text-2xl font-bold">
-            {sum?.toFixed(2)}€
-          </div>
-        </DisplayCard>
-        <DisplayCard title="Average" icon={Sigma}>
-          <div className="flex gap-2 items-center text-2xl font-bold">
-            {average.toFixed(2)}€
-          </div>
-        </DisplayCard>
-      </div>
       <TransactionChart transactions={transaction.all} />
       <TransactionTable transactions={transaction.all} />
     </div>
