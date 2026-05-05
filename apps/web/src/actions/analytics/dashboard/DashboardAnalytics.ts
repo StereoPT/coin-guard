59742ccf885495f65bb47ba4f317617e15729db7
StereoPT <stereopt@gmail.com>
2026-05-05 10:41:55 +0100

    refactor: action names

--- apps/web/src/actions/analytics/dashboard/DashboardAnalytics.ts
+++ apps/web/src/actions/analytics/dashboard/DashboardAnalytics.ts
@@ -2,7 +2,7 @@
 
 import { GetDashboardCategories } from "@/actions/analytics/dashboard/GetDashboardCategories";
 import { GetDashboardStats } from "@/actions/analytics/dashboard/GetDashboardStats";
-import { GetLastTransactions } from "@/actions/analytics/getLastTransactions";
+import { GetLastTransactions } from "@/actions/analytics/dashboard/GetLastTransactions";
 
 export const DashboardAnalytics = async () => {
   const stats = await GetDashboardStats();
