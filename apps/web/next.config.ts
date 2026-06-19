6d35b9c79b7faf7ce69aa3ebc8ce736fb71ff2cd
StereoPT <stereopt@gmail.com>
2026-06-19 16:08:52 +0100

    chore: automated versions

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,11 @@
+import pkg from "../../package.json";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@coin-guard/ui", "@coin-guard/db"],
+  env: {
+    NEXT_PUBLIC_APP_VERSION: pkg.version,
+  },
 };
 
 export default nextConfig;
