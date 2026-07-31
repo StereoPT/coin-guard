113e9819f486b36cdfc25c8355c8cacf055e440f
StereoPT <stereopt@gmail.com>
2026-07-31 10:43:17 +0100

    fix: transaction details description

--- apps/web/src/components/transactions/TransactionDetails.tsx
+++ apps/web/src/components/transactions/TransactionDetails.tsx
@@ -42,25 +42,23 @@ export const TransactionDetails = ({
     <div className="flex flex-col gap-6">
       <Card className="grid grid-cols-3">
         <div className="col-span-2">
-          <div className="flex justify-between items-start">
-            <CardHeader>
-              <CardDescription className="flex items-center gap-2 capitalize">
-                {transaction.transaction.type === TransactionType.CREDIT ? (
-                  <ArrowUpRight className="size-4" />
-                ) : (
-                  <ArrowDownRight className="size-4" />
-                )}
-                {transaction.transaction.type.toLowerCase()}
-              </CardDescription>
-              <CardTitle className="text-2xl font-semibold tabular-nums">
-                <CountUpWrapper
-                  type={CountType.MONEY}
-                  value={transaction.transaction.amount}
-                />
-              </CardTitle>
-              <CardTitle>{transaction.transaction.description}</CardTitle>
-            </CardHeader>
-          </div>
+          <CardHeader>
+            <CardDescription className="flex items-center gap-2 capitalize">
+              {transaction.transaction.type === TransactionType.CREDIT ? (
+                <ArrowUpRight className="size-4" />
+              ) : (
+                <ArrowDownRight className="size-4" />
+              )}
+              {transaction.transaction.type.toLowerCase()}
+            </CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums">
+              <CountUpWrapper
+                type={CountType.MONEY}
+                value={transaction.transaction.amount}
+              />
+            </CardTitle>
+            <CardTitle>{transaction.transaction.description}</CardTitle>
+          </CardHeader>
           <CardContent className="mt-6 grid grid-cols-3 gap-4 text-sm">
             <div className="flex flex-row gap-4 items-center">
               <BankAccountAvatar
